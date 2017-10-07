17752b0e32038397d113e4629e31b0d4408bbfa4
Keith Smith <aeiouandsometimesy@me.com>
2017-10-07 08:29:48 -0400

    Added description, needs comments

--- PRG-3-22-Angle-Calculator/main.cpp
+++ PRG-3-22-Angle-Calculator/main.cpp
@@ -5,6 +5,10 @@
 //  Created by Keith Smith on 10/7/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that asks the user for an angle, entered in radians. The program
+//  should then display the sine, cosine, and tangent of the angle. (Use the sin, cos, and
+//  tan library functions to determine these values.) The output should be displayed in
+//  fixed-point notation, rounded to four decimal places of precision.
 
 #include <iostream>
 #include <iomanip>
